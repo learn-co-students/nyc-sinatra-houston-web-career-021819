3fb2a6b1480386afe304987a0b31272c459161fb
moranja <jeffadammoran@gmail.com>
2019-03-13 15:02:33 -0500

    done

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,59 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  set :views, 'app/views/figures'
+  set :method_override, true
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :new
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :show
+  end
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :show_all
+  end
+
+  post '/figures' do
+    fig = Figure.create(params[:figure])
+    if params[:title][:name] != ""
+      new_title = Title.create(params[:title])
+      fig.titles << new_title
+    end
+    if params[:landmark][:name] != ""
+      new_landmark = Landmark.create(params[:landmark])
+      fig.landmarks << new_landmark
+    end
+    fig.save
+    redirect "/figures/#{fig.id}"
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :edit
+  end
+
+  patch '/figures/:id' do
+    fig = Figure.find(params[:id])
+    fig.update(params[:figure])
+    if params[:title][:name] != ""
+      new_title = Title.create(params[:title])
+      fig.titles << new_title
+    end
+    if params[:landmark][:name] != ""
+      new_landmark = Landmark.create(params[:landmark])
+      fig.landmarks << new_landmark
+    end
+    fig.save
+    redirect "/figures/#{fig.id}"
+  end
+
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,48 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+  set :views, 'app/views/landmarks'
+  set :method_override, true
+
+    get '/landmarks/new' do
+      @figures = Figure.all
+      erb :new
+    end
+
+    get '/landmarks/:id' do
+      @landmark = Landmark.find(params[:id])
+      @figures = Figure.all
+      erb :show
+    end
+
+    get '/landmarks' do
+      @landmarks = Landmark.all
+      erb :show_all
+    end
+
+    post '/landmarks' do
+      land = Landmark.create(params[:figure])
+      if params[:figure][:name] != ""
+        new_figure = Figure.create(params[:figure])
+        land.figures << new_figure
+      end
+      land.save
+      redirect "/landmarks/#{land.id}"
+    end
+
+    get '/landmarks/:id/edit' do
+      @landmark = Landmark.find(params[:id])
+      @figures = Figure.all
+      erb :edit
+    end
+
+    patch '/landmarks/:id' do
+      land = Landmark.find(params[:id])
+      land.update(params[:landmark])
+      if params[:figure][:name] != ""
+        new_figure = Figure.create(params[:figure])
+        land.figures << new_figure
+      end
+      land.save
+      redirect "/landmarks/#{land.id}"
+    end
+
 end
